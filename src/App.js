b9bfef7c5829be3856dc7771c8822bd77ea0056c
Anup <paddybaba@gmail.com>
2021-07-16 19:02:41 +0530

    navbar OK

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import './index.css'
 import Home from "./Home";
 import Contact from "./Contact";
 import TPA from "./TPA";
 import Departments from "./Departments";
 import OurDoctors from "./Our_Doctors";
+import Navbar from "./Navbar.jsx";
 
 import { Switch, Route, Redirect } from "react-router";
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={Contact} />
